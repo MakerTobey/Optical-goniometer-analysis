858f15dba3c851a30cc245016f62400c99387372
Tobias Wenzel <MakerTobey@users.noreply.github.com>
2017-09-03 17:09:40 +0100

    update
    renamed updated cleaned comments and unused code

--- Subroutines/NicePPlot.m
+++ Subroutines/NicePPlot.m
@@ -1,4 +1,4 @@
-function NicePPlotNoInterval(xAxis, yAxis, Data2D, cLims, xLabel, yLabel, zLabel, xTick, yTick, xTickLabel, Title, SaveName)
+function NicePPlot(xAxis, yAxis, Data2D, cLims, xLabel, yLabel, zLabel, xTick, yTick, xTickLabel, Title, SaveName)
 % make a publishable plot and save
 
 %% Common variables
@@ -8,18 +8,15 @@ FontName = 'MyriadPro-Regular'; % or choose any other font
 
 doExportPlot = true;
 
-% % Data2D = Data2D - min(min(Data2D)); % normalize to 0-1
-% % Data2D = Data2D / max(max(Data2D)); 
-
 
 %% start plot
 
-% figure dimensions in cm. I choose 1.5 or 2 times 
-% the target size typically. If figure is display in
+% figure dimensions in cm. If figure is display in
 % document much smaller increase the fontsize.
 
-figure_width = 10;  %14
+figure_width = 10;
 figure_height = 10;
+
 
 % --- setup plot windows
 figuresVisible = 'on'; % 'off' for non displayed plots (will still be exported)
@@ -34,22 +31,10 @@ hfig = figure(1); clf;
 
 % --- dimensions and position of plot 
 hsp = subplot(1,1,1, 'Parent', hfig);
-% % set(hsp,'Position',[0.15 0.17 0.75 0.80]);
-    
-%% plot
-% colorDepth = 1000;
-% colormap(jet(colorDepth));%jet  %%%% MAKE PLOT COLORFUL OR INTENSITY SCALED
-pcolor(xAxis, yAxis, Data2D)
+  
 
-%% Add vertical lines to the plot to mark change in intensities and label them
-% line('XData',[0, 0],'YData',[700, 700], ...
-% 	'linestyle', 'none', 'Color','r', ...
-%     'marker', 'p', 'markeredgecolor', 'none', 'markerfacecolor', 'w','MarkerSize', 12);%make marker
-%     %'Tag', '/4'
-% line('XData',[cosd(-2.5+270), cosd(-2.5+270)],'YData',[min(yAxis), max(yAxis)], ...
-%    'LineStyle', '--', 'LineWidth', 2, 'Color','w');
-% line('XData',[cosd(2.5+270), cosd(2.5+270)],'YData',[min(yAxis), max(yAxis)], ...
-%     'LineStyle', '--', 'LineWidth', 2, 'Color','w');%'k'
+%% plot
+pcolor(xAxis, yAxis, Data2D)
 
 
 %%  setup axis plot properties
@@ -88,8 +73,9 @@ else
     set(gca,'YTick',yTick)
 end
 
+
 %% label texts
-% xLabelText = %'x / µm';  % greek letters in LaTeX Syntax
+% xLabelText = %'x / Âµm';  % greek letters in LaTeX Syntax
 
 % save handles to set up label properties
 hTitle = title(Title);
@@ -116,11 +102,8 @@ set([gca, hcb, hTitle, hXLabel, hYLabel, hcLabel], ...
     'FontSize'   , FontSize    , ...
     'FontName'   , FontName);
 
+set(gca,'Layer', 'top'); % bring axis on top again (fix matlab bug)
 
-%% last 'bug' fixes
-
-% bring axis on top again (fix matlab bug)
-set(gca,'Layer', 'top');
 
 %% export
 drawnow
@@ -130,9 +113,8 @@ SaveDir = '';
 if (doExportPlot)
     IMAGENAME = [SaveDir SaveName]; 
     print(hfig, ['-r' num2str(400)], [IMAGENAME '.jpg' ], ['-d' 'jpeg']);
-%     print(hfig, ['-r' num2str(400)], [IMAGENAME '.svg' ], ['-d' 'svg']);  
-%     crop([IMAGENAME '.png']);
-%     display('finished plot export')
+    print(hfig, ['-r' num2str(400)], [IMAGENAME '.svg' ], ['-d' 'svg']);  
+    display('finished plot export')
 end
 
 % close(hfig);
